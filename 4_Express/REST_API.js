09c937c2bb6d63dfb485f16b0ca62fcb8da3cac8
Suman0703 <sumandevimehmi36@gmail.com>
2026-05-17 21:32:16 +0530

    Added dynamic user routes using app.route()

--- 4_Express/REST_API.js
+++ 4_Express/REST_API.js
@@ -12,20 +12,49 @@ app.get('/users', (req, res) => {
     const html = `
     <ul>
         ${users.map((user) => {
-            return `
+        return `
                 <li>${user.first_name}</li>
             `;
-        }).join("")}
+    }).join("")}
     </ul>
     `;
 
     res.send(html);
 });
 
+
+//JSON data
 app.get('/api/users', (req, res) => {
     return res.json(users);
 });
 
+//Dynamic Path
+// app.get('/api/users/:id', (req, res) => {
+//     const id = Number(req.params.id);
+//     const user = users.find((user) => user.id == id);
+//     return res.json(user);
+// });
+
+
+//Create New user
+app.post('/api/user', (req, res) => {
+    return res.json({ Status: "pending" })
+});
+
+app.route('/api/users/:id').get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id == id);
+    return res.json(user);
+}
+).patch((req, res) => {
+    //edit user route
+    res.json({ Status: Pending })
+}
+).delete((req, res) => {
+    //delete user route
+    res.json({ Status: Pending })
+}
+);
 
 app.listen(PORT, () => {
     console.log(`Server started at PORT:${PORT}`);
