23e4e4ca4e10b28ec83414cf8d410f72550bfea5
Suman0703 <sumandevimehmi36@gmail.com>
2026-05-18 12:31:36 +0530

    Implement CRUD operations using Express and local JSON file

--- 4_Express/REST_API.js
+++ 4_Express/REST_API.js
@@ -1,61 +1,114 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
 const PORT = 8000;
 
-// Routes
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-//html Client Side Rendering
-app.get('/users', (req, res) => {
+
+// HTML Client Side Rendering
+app.get("/users", (req, res) => {
 
     const html = `
     <ul>
-        ${users.map((user) => {
-        return `
-                <li>${user.first_name}</li>
-            `;
-    }).join("")}
+        ${users
+            .map((user) => {
+                return `
+                    <li>${user.first_name}</li>
+                `;
+            })
+            .join("")}
     </ul>
     `;
 
     res.send(html);
 });
 
-
-//JSON data
-app.get('/api/users', (req, res) => {
+// Get All Users (JSON Data)
+app.get("/api/users", (req, res) => {
     return res.json(users);
 });
 
-//Dynamic Path
-// app.get('/api/users/:id', (req, res) => {
+// Dynamic Path
+// app.get("/api/users/:id", (req, res) => {
 //     const id = Number(req.params.id);
 //     const user = users.find((user) => user.id == id);
+
 //     return res.json(user);
 // });
 
 
-//Create New user
-app.post('/api/user', (req, res) => {
-    return res.json({ Status: "pending" })
+// Create New User
+app.post("/api/users", (req, res) => {
+
+    const body = req.body;
+    users.push({ ...body, id: users.length + 1 });
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+
+        return res.json({
+            status: "Success", id: users.length
+        });
+    });
+
 });
 
-app.route('/api/users/:id').get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id == id);
-    return res.json(user);
-}
-).patch((req, res) => {
-    //edit user route
-    res.json({ Status: Pending })
-}
-).delete((req, res) => {
-    //delete user route
-    res.json({ Status: Pending })
-}
-);
+// Routes for Single User
+app.route("/api/users/:id")
 
+    // Get User By ID
+    .get((req, res) => {
+
+        const id = Number(req.params.id);
+
+        const user = users.find((user) => user.id == id);
+
+        return res.json(user);
+    })
+
+    // Update User
+    .patch((req, res) => {
+
+        const body = req.body;
+        const id = Number(req.params.id);
+
+        const user = users.find((user) => user.id == id);
+
+        users[users.indexOf(user)] = {
+            ...user,
+            ...body
+        };
+
+        fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+
+            return res.json({
+                status: "Success Data is updated",
+                id: users.length
+            });
+        });
+    })
+
+    // Delete User
+    .delete((req, res) => {
+
+        const id = Number(req.params.id);
+
+        const user = users.find((user) => user.id == id);
+
+        users.splice(users.indexOf(user), 1);
+
+        // Delete user route
+        return res.json({
+            status: "User's Data Deleted Successfully",
+        });
+    });
+
+
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server started at PORT:${PORT}`);
 });
